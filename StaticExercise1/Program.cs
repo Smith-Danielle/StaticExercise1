33f81eff288dcc1cff730cc567bcbf288da05ecc
Danielle Smith <smith.ddms@gmail.com>
2021-06-23 12:06:07 -0500

    Completed Static Exercise 1

---
+++ StaticExercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double converted = (TempConverter.FahrenheitToCelsius(100));
+            Console.WriteLine($"The conversion from Fahrenheit to Celsius is {converted}");
+
+            Console.WriteLine("Please enter a temperature in Celsius to convert to Fahrenheit:");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+            TempConverter.CelsiusToFahrenheit(userNumber);
+            
+        }
+    }
+}
---
+++ StaticExercise1/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StaticExercise1
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+
+            double celsiusConvert = (fahrenheit - 32) * (5.0 / 9.0);
+            
+            return celsiusConvert;
+        }
+        public static void CelsiusToFahrenheit(double celsius)
+        {
+            
+            double fahrenheitConvert = (celsius * (9.0 / 5.0)) + 32;
+
+            Console.WriteLine($"The Fahrenheit is {fahrenheitConvert}");
+        }
+    }
+}
